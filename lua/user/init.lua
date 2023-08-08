20a06ff3f79f871012aff652df04fe6a2f8e5fc6
PAFFx <jj2jom@gmail.com>
2023-08-08 13:55:23 +0700

    fix clangd multiple client encoding problem

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -83,6 +83,16 @@ return {
     require("notify").setup({
       background_colour = "#000000",
     })
+
+    local cmp_nvim_lsp = require "cmp_nvim_lsp"
+    require("lspconfig").clangd.setup {
+      on_attach = on_attach,
+      capabilities = cmp_nvim_lsp.default_capabilities(),
+      cmd = {
+        "clangd",
+        "--offset-encoding=utf-16"
+      }
+    }
   end,
 
   -- This function run alpha-nvim when there is no more buffer
