95ef12a97041700dfd5cadce4c6276e9e9887960
Jittabun <Jittabun.ba@chococrm.com>
2026-02-11 17:10:18 +0700

    feat: cursor smear

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -97,4 +97,22 @@ return {
       )
     end,
   },
+
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      stiffness = 0.8, -- 0.6      [0, 1]
+      trailing_stiffness = 0.6, -- 0.45     [0, 1]
+      stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+      trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+      damping = 0.95, -- 0.85     [0, 1]
+      damping_insert_mode = 0.95, -- 0.9      [0, 1]
+      distance_stop_animating = 0.5, -- 0.1      > 0
+      time_interval = 7, -- milliseconds
+      smear_between_neighbor_lines = false,
+      smear_insert_mode = false,
+      cursor_color = "#fb4c5d",
+      legacy_computing_symbols_support = true,
+    },
+  },
 }
